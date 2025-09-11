b2a3ba5e90105585829c19e81f2adefbfe8050c2
Rachel-Liu221 <LIUYUC@bc.edu>
2025-09-10 22:34:02 -0400

    add part3 and part4

--- ps1-part3.py
+++ ps1-part3.py
@@ -1,1 +1,17 @@
+def heart_rate(age, goal):
+    max_HR = 220 - age
+    print("Your maximum heart rate is:", max_HR)
+
+    if goal == "S":
+        low = 0.8 * max_HR
+        print("Your target heart rate is between", low, "and", max_HR)
+    elif goal == "E":
+        low = 0.6 * max_HR
+        high = 0.8 * max_HR
+        print("Your target heart rate is between", low, "and", high)
+
+user_age = int(input("What is your age? "))
+user_goal = input("Is your goal speed (S) or endurance (E)? ")
+
+heart_rate(user_age, user_goal)
 
--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,11 @@
+prior_arrests = int(input("Prior arrests: "))
+ordinance = input("Prior arrests for local ordinance (Y/N): ")
+age_release = int(input("Age at release: "))
 
+score = prior_arrests
+if ordinance == "N":
+    score -= 1
+if age_release > 40:
+    score -= 1
+
+print("The recidivism risk score is", score)
